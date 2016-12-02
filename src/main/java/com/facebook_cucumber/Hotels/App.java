6c7c42c52b186c58570eb1be325e5b15d6d1cf01
abehl <rubyzainubbehl@gmail.com>
2016-12-01 21:16:07 -0500

    Initializing my repo

---
+++ src/main/java/com/facebook_cucumber/Hotels/App.java
@@ -0,0 +1,13 @@
+package com.facebook_cucumber.Hotels;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
